09396b48a5c9096dc940fad021e144f40aeb3171
Folasade Ekulona <ekulonaf@gmail.com>
2023-04-02 20:21:28 -0400

    adds completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+const num3 = 22;
+const num4 = 18;
+const mod = num3 % num4
+
+let max = Math.max(1, 2, 3, 5, 6, 7,8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
